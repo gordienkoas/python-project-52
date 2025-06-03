d889b309fdc2210ebf9ace566552fed78a9be0dd
user <gordienkoas@bk.ru>
2025-06-03 11:08:21 +0000

    step 2 done

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 from .views import users_view, register_view  # Импорт функций-представлений
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('register/', register_view, name='register'),  # без .as_view()
     path('users/', users_view, name='users'),           # без .as_view()
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 
 def home_view(request):
     return render(request, 'index.html')
@@ -22,3 +23,19 @@ def users_view(request):
     from django.contrib.auth.models import User
     users = User.objects.all()
     return render(request, 'users.html', {'users': users})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+    else:
+        form = AuthenticationForm()
+    # Добавляем класс 'form-control' к каждому полю
+    for field in form.fields.values():
+        field.widget.attrs.update({'class': 'form-control'})
+
+    if request.method == 'POST' and form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        return redirect('home')
+    return render(request, 'login.html', {'form': form})
