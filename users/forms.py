79195d9ae28c991301e12620620755c2aeedfa06
user <gordienkoas@bk.ru>
2025-06-18 12:53:05 +0000

    update apps

---
+++ users/forms.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+from django import forms
+
+
+
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+class UserUpdateForm(UserChangeForm):
+    password = None  # не показываем поле пароля для редактирования
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+
+
+
