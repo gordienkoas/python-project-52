aad22915758a760f794ebd671a476806b2735387
user <gordienkoas@bk.ru>
2025-06-18 14:33:59 +0000

    update apps

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
-# скачиваем uv
-curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
+set -e
 
-make install && make migrate
+# Скачиваем и устанавливаем uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source "$HOME/.local/bin/env"
+
+# Создаём виртуальное окружение, если его нет
+if [ ! -d ".venv" ]; then
+  python3 -m venv .venv
+fi
+
+# Активируем виртуальное окружение
+source .venv/bin/activate
+
+
+make install
+make migrate
