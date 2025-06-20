f6eced4035b72493e42cff177359395497a0dfec
user <gordienkoas@bk.ru>
2025-06-20 18:40:38 +0000

    change views.py

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -9,43 +9,24 @@ class UserRegisterForm(UserCreationForm):
         label=_("Имя"),
         max_length=30,
         required=True,
-        widget=forms.TextInput(attrs={
-            'placeholder': 'Имя',
-            # 'name': 'first_name'  # обычно не обязательно добавлять вручную
-        }),
+        widget=forms.TextInput(attrs={'placeholder': 'Имя'}),
     )
     last_name = forms.CharField(
         label=_("Фамилия"),
         max_length=30,
         required=True,
-        widget=forms.TextInput(attrs={
-            'placeholder': 'Фамилия',
-        }),
+        widget=forms.TextInput(attrs={'placeholder': 'Фамилия'}),
     )
-    username = forms.CharField(
-        label=_("Имя пользователя"),
-        max_length=150,
-        required=True,
-        widget=forms.TextInput(attrs={
-            'placeholder': 'Имя пользователя',
-        }),
-    )
-    password1 = forms.CharField(
-        label=_("Пароль"),
-        strip=False,
-        required=True,
-        widget=forms.PasswordInput(attrs={
-            'placeholder': 'Пароль',
-        }),
-    )
-    password2 = forms.CharField(
-        label=_("Подтверждение пароля"),
-        strip=False,
-        required=True,
-        widget=forms.PasswordInput(attrs={
-            'placeholder': 'Подтверждение пароля',
-        }),
-    )
+    # остальные поля ...
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        if commit:
+            user.save()
+        return user
+
 
 
 class UserUpdateForm(forms.ModelForm):
