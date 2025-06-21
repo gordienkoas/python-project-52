72ad3654076188ecbcb1db58ad8bf31886e8553b
user <gordienkoas@bk.ru>
2025-06-21 19:26:02 +0000

    update task models

--- task_manager/tasks/forms.py
+++ task_manager/tasks/forms.py
@@ -3,13 +3,6 @@ from .models import Task
 from django import forms
 from django.utils.translation import gettext_lazy as _
 
-DATA = {
-    "tasks": {
-        "first": {
-            "executor": "Выберите исполнителя"
-        }
-    }
-}
 
 class CreateTaskForm(ModelForm):
     description = forms.CharField(
@@ -18,14 +11,6 @@ class CreateTaskForm(ModelForm):
             attrs={"style": "height: 13em; resize: none;"},
         ),
     )
-
-    def get_form(self, form_class=None):
-        form = super().get_form(form_class)
-        if 'executor' in form.fields:
-            choices = list(form.fields['executor'].choices)
-            choices.append(('special_value', DATA["tasks"]["first"]["executor"]))
-            form.fields['executor'].choices = choices
-        return form
 
     class Meta:
         model = Task
