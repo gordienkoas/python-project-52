adc0652861c70e41070896568205a51e72902e6e
user <gordienkoas@bk.ru>
2025-06-21 10:23:13 +0000

    add labels create.html

--- task_manager/labels/models.py
+++ task_manager/labels/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 class Label(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=40, unique=True, null=False)
+    created_at = models.DateTimeField(_("Creation date"), auto_now_add=True)
 
     def __str__(self):
         return self.name
